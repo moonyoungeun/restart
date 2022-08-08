807fc17d79ecd96e7da1aa5e5c0390deb011ccd0
moonyoungeun <pdu04223@naver.com>
2022-08-08 20:33:12 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var s = skrollr.init();
+
+window.addEventListener("scroll", function () {
+  // 스크롤 위치값
+  console.log(scrollY);
+});
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// gsap.defaults({
+//   ease: "none",
+// });
+
+// let tl = gsap.timeline();
+
+// tl.to(".main_text", {
+//   scrollTrigger: {
+//     trigger: ".main_text",
+//     scrub: true,
+//     markers: true,
+//     start: "300 650",
+//     pin: true,
+//   },
+//   x: -500,
+// });
+
+// tl.to(".main_text2", {
+//   scrollTrigger: {
+//     trigger: ".main_text2",
+//     scrub: true,
+//     markers: true,
+//     start: "-300 500",
+//     pin: true,
+//   },
+//   x: 500,
+// });
